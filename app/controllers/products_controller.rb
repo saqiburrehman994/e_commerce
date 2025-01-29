23ee34c1395d91d33860b5d0940aaa8cefb59547
saqiburrehman994 <saqibrehman062@gmail.com>
2025-01-29 17:52:42 +0500

    add image to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,7 +44,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :stock_quantity)
+    params.require(:product).permit(:name, :description, :price, :stock_quantity,:image)
   end
 
   def set_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :user, optional: true
+  has_one_attached :image
 end
