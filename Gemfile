9218be57467fafe9b9c956a7f2d47c5aee8e7937
saqiburrehman994 <saqibrehman062@gmail.com>
2025-01-29 13:30:42 +0500

    delivery of forgot_password_email

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem "jbuilder"
 gem "bootstrap"
 gem "sassc-rails"
 gem "devise"
+gem "dotenv-rails"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,16 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true
+}
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
