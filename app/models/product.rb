f523f2fd24d626dc6eb9bc09cc467e98d5102e21
saqiburrehman994 <saqibrehman062@gmail.com>
2025-01-31 12:39:24 +0500

    validates product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,9 @@ class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0}
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 end
