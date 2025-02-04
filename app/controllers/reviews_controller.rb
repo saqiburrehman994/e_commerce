d7de30bf67a6f4ea0c12889b514374513fd95e74
saqiburrehman994 <saqibrehman062@gmail.com>
2025-02-04 12:44:38 +0500

    add reviews to only purchased products

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,21 @@
 class ReviewsController < ApplicationController
   before_action :find_product
+  before_action :enusure_purchased!, only: [:new,:create]
+
   def new
     @review = @product.reviews.new
   end
   def create
-    @review = @product.reviews.new(review_params)
-    @review.user = current_user
-    if @review.save
-      respond_to do |format|
-        format.turbo_stream
+      @review = @product.reviews.new(review_params)
+      @review.user = current_user
+      if @review.save
+        respond_to do |format|
+          format.turbo_stream
+        end
+      else
+        flash[:alert] = "Failed to add review."
+        redirect_to product_path(@product)
       end
-    else
-      flash[:alert] = "Failed to add review."
-      redirect_to product_path(@product)
-    end
   end
 
   def edit
@@ -44,7 +46,14 @@ class ReviewsController < ApplicationController
     @product = Product.find(params[:product_id])
   end
 
+  def enusure_purchased!
+    unless current_user.purchased?(@product)
+      redirect_to product_path(@product), alert: "You can only add reviews if you have purchased this product."
+    end
+  end
+
   def review_params
     params.require(:review).permit(:rating,:comment)
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
   has_one  :shipping_detail, dependent: :destroy
   has_one  :payment_detail
   has_many :reviews, dependent: :destroy
+
+
+  def purchased?(product)
+      orders.joins(:order_items).exists?( order_items: { product_id: product.id})
+  end
 end
